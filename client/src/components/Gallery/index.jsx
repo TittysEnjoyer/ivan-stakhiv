215b87fa108d375915c3763568ef1e14c24d501d
TittysEnjoyer <vanoklosok1@gmail.com>
2024-11-27 10:59:58 +0200

    update

--- client/src/components/Gallery/index.jsx
+++ client/src/components/Gallery/index.jsx
@@ -8,28 +8,28 @@ const Gallery = () => {
             <div className="gallery-wrapper">
                 <div className="gallery-card">
                     <img className="gallery-card-image" src="/assets/images/1.png" alt="" />
-                    <h2 className="txt-title-primary gallery-card-title">Я і бал принцес</h2>
-                    <span className="txt-desc-primary gallery-card-desc">Interactive content with vibrant animations.</span>
+                    <h2 className="txt-title-primary gallery-card-title">Швидко</h2>
+                    <span className="txt-desc-primary gallery-card-desc">Знайдіть відповідь на своє питання в декілька кліків.</span>
                 </div>
                 <div className="gallery-card">
                     <img className="gallery-card-image" src="/assets/images/2.png" alt="" />
-                    <h2 className="txt-title-primary gallery-card-title">Education</h2>
-                    <span className="txt-desc-primary gallery-card-desc">Interactive content with vibrant animations.</span>
+                    <h2 className="txt-title-primary gallery-card-title">Зручно</h2>
+                    <span className="txt-desc-primary gallery-card-desc">На нашоииу сайті дуже проста навігація.</span>
                 </div>
                 <div className="gallery-card">
                     <img className="gallery-card-image" src="/assets/images/3.png" alt="" />
-                    <h2 className="txt-title-primary gallery-card-title">Education</h2>
-                    <span className="txt-desc-primary gallery-card-desc">Interactive content with vibrant animations.</span>
+                    <h2 className="txt-title-primary gallery-card-title">Безпечно</h2>
+                    <span className="txt-desc-primary gallery-card-desc">Сайт надійно захищений від хакерів.</span>
                 </div>
                 <div className="gallery-card">
                     <img className="gallery-card-image" src="/assets/images/4.png" alt="" />
-                    <h2 className="txt-title-primary gallery-card-title">Education</h2>
-                    <span className="txt-desc-primary gallery-card-desc">Interactive content with vibrant animations.</span>
+                    <h2 className="txt-title-primary gallery-card-title">Пізнавально</h2>
+                    <span className="txt-desc-primary gallery-card-desc">Читайте та відкривайте для себе щось нове.</span>
                 </div>
                 <div className="gallery-card">
                     <img className="gallery-card-image" src="/assets/images/5.png" alt="" />
-                    <h2 className="txt-title-primary gallery-card-title">Education</h2>
-                    <span className="txt-desc-primary gallery-card-desc">Interactive content with vibrant animations.</span>
+                    <h2 className="txt-title-primary gallery-card-title">Зворотний зв'язок</h2>
+                    <span className="txt-desc-primary gallery-card-desc">Питання, скарги та пропозиція у вкладці 'About me'.</span>
                 </div>
             </div>
         </div>
